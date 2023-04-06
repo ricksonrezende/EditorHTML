0af98430dabafa928c842f3c8eea407d51c6f969
ricksonrezende <ricksonrezende1@gmail.com>
2023-04-05 22:19:32 -0300

    Iniciando o projeto

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace MyApp
+namespace EditorHTML
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-
+            Menu.Show();
         }
     }
 }

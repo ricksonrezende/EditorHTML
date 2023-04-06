26ac355edba09034bf36d30a127af5119157e766
ricksonrezende <ricksonrezende1@gmail.com>
2023-04-05 23:24:43 -0300

    Menu da aplicação

--- Menu.cs
+++ Menu.cs
@@ -14,6 +14,7 @@ namespace EditorHTML
             WriteOptions();
 
             var option = short.Parse(Console.ReadLine());
+            HandleMenuOption(option);
         }
 
         public static void Draw()
@@ -60,6 +61,22 @@ namespace EditorHTML
             Console.WriteLine("0 - Sair");
             Console.SetCursorPosition(3, 10);
             Console.WriteLine("Opção: ");
+        }
+
+        public static void HandleMenuOption(short option)
+        {
+            switch(option)
+            {
+                case 1: Console.WriteLine("Editor"); break;
+                case 2: Console.WriteLine("View"); break;
+                case 0: 
+                {
+                    Console.Clear();
+                    Environment.Exit(0);
+                    break;
+                }
+                default: Show(); break;
+            }
         }
     }
 }

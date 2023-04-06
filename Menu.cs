74bc7a97022622914802f2433c6e7f6483f5623d
ricksonrezende <ricksonrezende1@gmail.com>
2023-04-05 23:17:51 -0300

    Exibindo o menu

--- Menu.cs
+++ Menu.cs
@@ -11,6 +11,9 @@ namespace EditorHTML
             Console.ForegroundColor = ConsoleColor.White;
 
             DrawScreen();
+            WriteOptions();
+
+            var option = short.Parse(Console.ReadLine());
         }
 
         public static void Draw()
@@ -39,6 +42,24 @@ namespace EditorHTML
 
             Draw();
 
+        }
+
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(3, 2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("===========================");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Selecione uma opção abaixo:");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Novo arquivo");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Abrir arquivo");
+            Console.SetCursorPosition(3, 9);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(3, 10);
+            Console.WriteLine("Opção: ");
         }
     }
 }

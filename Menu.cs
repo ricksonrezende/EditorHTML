f20f2c98c0401c71c0a4ece2075b38eb8c1b612c
ricksonrezende <ricksonrezende1@gmail.com>
2023-04-05 22:55:17 -0300

    Desenhando a tela

--- Menu.cs
+++ Menu.cs
@@ -9,6 +9,36 @@ namespace EditorHTML
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.DarkBlue;
             Console.ForegroundColor = ConsoleColor.White;
+
+            DrawScreen();
+        }
+
+        public static void Draw()
+        {
+            Console.Write("+");
+            for (int i = 0; i <= 30; i++)
+                Console.Write("-");
+
+            Console.Write("+");
+            Console.Write("\n");
+        }
+
+        public static void DrawScreen()
+        {
+            Draw();
+
+            for (int lines = 0; lines <= 10; lines++)
+            {
+                Console.Write("|");
+                for (int i = 0; i <= 30; i++)
+                    Console.Write(" ");
+
+                Console.Write("|");
+                Console.Write("\n");
+            }
+
+            Draw();
+
         }
     }
 }
